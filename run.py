92d2f10dd462b731899995da55012562183dd046
yangxuekang <yangxuekang@sjtu.edu.cn>
2026-06-10 11:11:22 +0800

    refactor: use shared/waypoint_prediction instead of local copy

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'shared'))
 import torch
 import random
 import argparse
